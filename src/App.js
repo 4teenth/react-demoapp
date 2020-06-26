3cc6ec66c48d59abbbdda954fbd5ef9f59f72134
4teenth <aio.ao91@gmail.com>
2020-06-26 04:58:12 +0300

    complete

--- src/App.js
+++ src/App.js
@@ -1,16 +1,68 @@
 import React from "react";
+import Logo from "./components/Logo";
 //RULE!!! One file - one component
-import Painting from "./components/Painting";
+// import Painting from "./components/Painting";
+import PaintingList from "./components/PaintingList";
+import Panel from "./components/Panel";
 import paintings from "./paintings.json";
 
-console.log(paintings);
+// const numbers = [1, 2, 3, 4, 5];
+// console.log(numbers.map((number) => <div>{number}</div>));
+
+// console.log(paintings);
+
+// const colors = ["red", "green", "blue", "purple"];
 
 //Пропсы передаются от туда, от куда мы рендерим компонент, а используются в самом компоненте (e.g. Painting)
 const App = () => {
   return (
     <div>
-      <h1>Главный компонент-контейнер приложения</h1>
-      <Painting
+      <Panel title="Latest news">
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate,
+          fugit! Quam quae voluptates libero delectus. Amet provident eaque
+          dicta facere sapiente, impedit officia laboriosam nulla distinctio
+          optio tempora rem repudiandae.
+        </p>
+        <a href="">Read more here...</a>
+      </Panel>
+
+      <Panel>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+          tenetur, aspernatur corporis a aut eos cumque, sed cum animi nesciunt
+          delectus quia! Non, atque. Quasi veritatis dolore saepe sed itaque.
+        </p>
+      </Panel>
+
+      {/* <h1>Главный компонент-контейнер приложения</h1> */}
+      <Logo text="Главный компонент-контейнер приложения" />
+
+      <PaintingList paintingsProps={paintings} />
+
+      {/* рендеp коллекции */}
+      {/* <ul>
+        {colors.map((color) => (
+          <li key={color}>{color}</li>
+        ))}
+      </ul> */}
+
+      {/* <ul>
+        {paintings.map((painting) => (
+          <li key={painting.id}>
+            <Painting
+              url={painting.url}
+              title={painting.title}
+              price={painting.price}
+              profileUrl={painting.author.url}
+              tag={painting.author.tag}
+              quantity={painting.quantity}
+            />
+          </li>
+        ))}
+      </ul> */}
+
+      {/* <Painting
         url={paintings[0].url}
         title={paintings[0].title}
         price={paintings[0].price}
@@ -28,7 +80,7 @@ const App = () => {
         profileUrl="https://redditery.com/user/brenunsk/submitted"
         tag="brenunsk"
         quantity={3 + 1}
-      />
+      /> */}
     </div>
   );
 };
